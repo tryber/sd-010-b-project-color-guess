8d752a75d81807d54ed390063334333d50112d17
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-20 18:04:24 -0300

    ajustando geracao de cores

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//gerando rgb
+let rgb = document.getElementById('rgb-color');
+rgb.innerText = '(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', '+ Math.floor(Math.random() * 255) + ')';
+
 //Gerando os 6 circulos
 for(let i = 0; i < 6; i += 1) {
   let bolaDeCor = document.createElement('li');
@@ -7,11 +11,14 @@ for(let i = 0; i < 6; i += 1) {
 }
 
 //Colocando cores nos circulos
+let circulos = document.getElementsByClassName('ball');
+
 function adicionandoCor() {
-  let circulos = document.getElementsByClassName('ball');
   for (let i = 0; i < circulos.length; i += 1) {
     circulos[i].style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', '+ Math.floor(Math.random() * 255) + ')';
   }
+  let escolhendoCirculoAleatorio = circulos[Math.floor(Math.random() * circulos.length)];
+  escolhendoCirculoAleatorio.style.backgroundColor = `rgb${rgb.innerText}`;
 }
 
 window.onload = adicionandoCor;
