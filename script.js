4b62d8dc47be5347a731950ed5971aa98e023e74
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-20 17:11:46 -0300

    colocando os circulos

---
+++ script.js
@@ -0,0 +1,6 @@
+for(let i = 0; i < 6; i += 1) {
+  let bolaDeCor = document.createElement('li');
+  bolaDeCor.className = 'bola-de-cor';
+  let paletaDeCores = document.getElementById('paleta-de-cores');
+  paletaDeCores.appendChild(bolaDeCor);
+}
