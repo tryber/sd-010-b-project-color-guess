9f337f1ca61e055772adbea69062cb801e4bcace
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-21 19:53:09 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -6,19 +6,42 @@ rgb.innerText = '(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.r
 for(let i = 0; i < 6; i += 1) {
   let bolaDeCor = document.createElement('li');
   bolaDeCor.className = 'ball';
+  bolaDeCor.classList.add('errado');
   let paletaDeCores = document.getElementById('paleta-de-cores');
   paletaDeCores.appendChild(bolaDeCor);
 }
 
 //Colocando cores nos circulos
 let circulos = document.getElementsByClassName('ball');
+let escolhendoCirculoAleatorio = circulos[Math.floor(Math.random() * circulos.length)];
 
 function adicionandoCor() {
   for (let i = 0; i < circulos.length; i += 1) {
     circulos[i].style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', '+ Math.floor(Math.random() * 255) + ')';
-  }
-  let escolhendoCirculoAleatorio = circulos[Math.floor(Math.random() * circulos.length)];
+  } 
+  escolhendoCirculoAleatorio.classList.remove('errado');
   escolhendoCirculoAleatorio.style.backgroundColor = `rgb${rgb.innerText}`;
+  escolhendoCirculoAleatorio.id = 'answer'
 }
+adicionandoCor();
 
-window.onload = adicionandoCor;
+//escolhendo a cor certa
+let resultado = document.getElementById('resultado');
+let corCerta = document.getElementById('answer');
+let corErrada = document.getElementsByClassName('errado');
+
+corCerta.addEventListener('click', function() {
+  resultado.innerText = 'Acertou!';
+});
+
+for(let i = 0; i < corErrada.length; i += 1) {
+  corErrada[i].addEventListener('click', function() {
+    resultado.innerText = 'Errou! Tente novamente!';
+  });
+};
+
+
+
+
+
+
